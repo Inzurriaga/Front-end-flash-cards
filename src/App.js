9a2060c879063ffb4877fba7301495f78d71ecfc
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-01-04 21:04:44 -0700

    Hamburger menu animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import './normalize.css'
+// import MainMenu from './mainmenu.js'
 
 class App extends Component {
 
   toggleMenu() {
-    let page = document.querySelector('.page');
+    let page = document.querySelector(".page");
+    let sticks = document.querySelectorAll(".stick")
       if(!page.classList.contains("what")){
         page.classList.add("what");
       }else{
         page.classList.remove("what");
-      }   
+      }
+      sticks.forEach((stick) => {
+      if(!stick.classList.contains("open") || stick.classList.contains("close"))  {
+        stick.classList.remove("close");
+        stick.classList.add("open")
+      } else{
+        stick.classList.remove("open");
+        stick.classList.add("close")
+      }
+    })
   }
 
   render() {
@@ -18,9 +29,11 @@ class App extends Component {
       <div className="page">
         <div onClick={this.toggleMenu} className="hamburger-container">
           <div className="hamburger-circle"></div>
-          <div className="stick stick-one"></div>
-          <div className="stick stick-two"></div>
-          <div className="stick stick-three"></div>
+          <div className="stick-container">
+            <div className="stick stick-one"></div>
+            <div className="stick stick-two"></div>
+            <div className="stick stick-three"></div>
+          </div>
         </div>
         <ul className="nav">
           <li>Main Menu</li>
@@ -31,7 +44,7 @@ class App extends Component {
           <li>Practice</li>
         </ul>
         <section className="main-content">
-          <h1> im the practice text to see if this works</h1>
+          {/* <MainMenu /> */}
         </section>
       </div>
     );
