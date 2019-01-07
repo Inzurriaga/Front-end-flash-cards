ccb3c6e240bd2ddd070e2809237c8b721ed96071
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-01-07 13:23:53 -0700

    css for create is added

--- src/CreateCard.js
+++ src/CreateCard.js
@@ -6,7 +6,7 @@ class Card {
         this.catagory = "personal"
         this.question = question;
         this.answer = answer;
-        this.color = color;
+        this.color = color || "#ffffff";
     }
 }
 
@@ -18,30 +18,50 @@ class CreateCard extends Component {
         }
     }
 
-    createCard() {
-        let answer = document.querySelector(".");
-        let question = document.querySelector(".");
+    createCard = () => {
+        let personalArray = this.state.personalDeck;
+        let answer = document.querySelector(".answer-text").value;
+        let question = document.querySelector(".question-text").value;
+        let color = document.querySelector(".create-card-input").checked.value
+        let card = new Card(answer, question, color)
+        personalArray.push(card)  
+        this.setState({ personalDeck: personalArray })
+        localStorage.setItem('personalDeck', JSON.stringify(this.state.personalDeck));
     }
+
     render() {
         return (
-            <form>
-                <fieldset>
-                    <legend>Create a Card</legend>
-                    <div className="create-card">
-                        <label>Create Question/Defintion</label>
-                        <textarea></textarea>
+            <section>
+                    <h2>Create a Card</h2>
+                    <div className="create-card-container">
+                        <div className="create-card">
+                            <h4>Create Question/Defintion</h4>
+                            <textarea className="question-text"></textarea>
+                        </div>
+                        <div className="create-card">
+                            <h4>Create Answer/Word</h4>
+                            <textarea className="answer-text"></textarea>
+                        </div>
                     </div>
-                    <div className="create-card">
-                        <label>Create Answer/Word</label>
-                        <textarea></textarea>
+                    <h3 className="pick-label">Pick a color</h3>
+                    <div className="radio-button">
+                        <input className="create-card-input" type="radio" value="orange" name="card-color"/>
+                        <span className="radio-selected"></span>
                     </div>
-                    <label>Pick a color</label>
-                    <input type="radio" name="card-color"/>
-                    <input type="radio" name="card-color"/>
-                    <input type="radio" name="card-color"/>
-                    <input type="radio" name="card-color"/>
-                </fieldset>
-            </form>
+                    <div className="radio-button">
+                        <input className="create-card-input" type="radio" value="blue" name="card-color"/>
+                        <span className="radio-selected"></span>
+                    </div>
+                    <div className="radio-button">
+                        <input className="create-card-input" type="radio" value="red" name="card-color"/>
+                        <span className="radio-selected"></span>
+                    </div>
+                    <div className="radio-button">
+                        <input className="create-card-input" type="radio" value="yellow" name="card-color"/>
+                        <span className="radio-selected"></span>
+                    </div>
+                    <button onClick={this.createCard} className="create-card-button">Create Card</button>
+            </section>
         )
     }
 }
